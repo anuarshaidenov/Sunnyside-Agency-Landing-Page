10ca098c02b201c006e89f0057c9a8f2a929dcbb
Anuar Shaidenov <anuarshaidenov@gmail.com>
2021-09-18 00:36:27 +0600

    add backdrop and closing menu functionality on click

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const hamburger = document.querySelector('.nav--menu-icon');
 const mobileMenu = document.querySelector('.mobile-nav-elements');
+const backdrop = document.querySelector('.backdrop');
 
 hamburger.addEventListener('click', function(){
     mobileMenu.classList.toggle('hidden');
+    backdrop.classList.toggle('hidden');
+})
+
+backdrop.addEventListener('click', function(){
+    mobileMenu.classList.toggle('hidden');
+    backdrop.classList.toggle('hidden');
 })
